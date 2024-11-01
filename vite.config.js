2c949ad50e75cfbd5bbaf2109a500d105189fbb0
estif4 <estifk6@gmail.com>
2024-11-01 10:22:21 +0300

    secont commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // Replace 4000 with your preferred port
+    port: 3000, // Replace 3000 with your preferred port
   },
-})
+  base: '/TicTacToe_Game/', // Set the base path to your GitHub repository name
+});
